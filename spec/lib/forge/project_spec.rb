1766e354c40f012697a34b09447d580e2e2b3179
Jason Webster <jason@metalabdesign.com>
2013-09-21 15:44:07 -0700

    Cleanup specs & spec helper

--- spec/lib/forge/project_spec.rb
+++ spec/lib/forge/project_spec.rb
@@ -1,31 +1,30 @@
-require 'forge/project'
+require 'spec_helper'
 
 describe Forge::Project do
-  before(:each) do
-    @project = Forge::Project.new('/tmp/', nil, {:name => 'Hello'})
-  end
+
+  let(:project) { Forge::Project.new('/tmp/', nil, {:name => 'Hello'}) }
 
   describe :config_file do
     it "should create an expanded path to the config file" do
-      @project.config_file.to_s.should == '/tmp/config.rb'
+      project.config_file.to_s.should == '/tmp/config.rb'
     end
   end
 
   describe :assets_path do
     it "should create an expanded path to the assets folder" do
-      @project.assets_path.to_s.should == '/tmp/source/assets'
+      project.assets_path.to_s.should == '/tmp/source/assets'
     end
   end
 
   describe :build_dir do
     it "should create an expanded path to the forge build directory" do
-      @project.build_path.to_s.should === '/tmp/.forge/build'
+      project.build_path.to_s.should == '/tmp/.forge/build'
     end
   end
 
   describe :theme_id do
     it "should be the same as the project folder" do
-      @project.theme_id.should == 'tmp'
+      project.theme_id.should == 'tmp'
     end
   end
 
